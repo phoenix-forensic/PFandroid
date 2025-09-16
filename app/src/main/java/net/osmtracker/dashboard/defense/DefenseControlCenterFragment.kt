a6f7b1bee23b1849be6610117ae17b1f802c2dc7
PHOENIX <gitdev00@gmail.com>
2025-09-15 22:03:23 -0300

    Add analytics dashboard screens

---
+++ app/src/main/java/net/osmtracker/dashboard/defense/DefenseControlCenterFragment.kt
@@ -0,0 +1,6 @@
+package net.osmtracker.dashboard.defense
+
+import androidx.fragment.app.Fragment
+import net.osmtracker.R
+
+class DefenseControlCenterFragment : Fragment(R.layout.fragment_defense_control_center)
